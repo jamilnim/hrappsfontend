39bfea678453508cb50edb8edd89969efdbbdfe9
jamilnim <jamilnimbook2@gmail.com>
2025-06-16 22:34:29 +0300

    heade recreate

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,31 +1,25 @@
-import React from "react";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink, Link } from "react-router";
 import Em from "../../assets/Em.png";
 import styles from "./Header.module.css";
 
+import React from "react";
+
 function Header({ name }) {
   return (
-    <header className={styles.header}>
-      <div className={styles.logoContainer}>
-        <img className={styles.logo} src={Em} alt="logo" />
-        <Link to="/">
-          <h2>{name}</h2>
-        </Link>
-      </div>
-      <nav>
-        <ul className={styles.navList}>
-          <li>
+    <div>
+      <header className={styles.header}>
+        <div className="logo">
+          <img className={styles.logo} src={Em} alt="logo" />
+        </div>
+        <nav>
+          <ul>
             <NavLink to="/about">About</NavLink>
-          </li>
-          <li>
             <NavLink to="/employeelist">Employee list</NavLink>
-          </li>
-          <li>
             <NavLink to="/add">Add Employee</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
+          </ul>
+        </nav>
+      </header>
+    </div>
   );
 }
 
