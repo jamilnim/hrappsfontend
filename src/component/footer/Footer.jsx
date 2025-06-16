69b15093231c77e19fd7ebb95cbd2c82e80e6701
jamilnim <jamilnimbook2@gmail.com>
2025-06-16 20:55:53 +0300

    header css

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,7 +1,6 @@
 import { NavLink, Link } from "react-router";
-
-import styles from "./Footer.module.css";
 import Em from "../../assets/Em.png";
+import styles from "./Footer.module.css";
 
 import React from "react";
 
@@ -10,15 +9,13 @@ function Footer({ name }) {
     <div>
       <footer className={styles.footer}>
         <div className="logo">
-           <img className={styles.logo} src={Em} alt="logo" />
-         
+          <img className={styles.logo} src={Em} alt="logo" />
         </div>
         <nav>
           <ul>
             <NavLink to="/about">About</NavLink>
             <NavLink to="/employeelist">Employee list</NavLink>
             <NavLink to="/add">Add Employee</NavLink>
-      
           </ul>
         </nav>
       </footer>
