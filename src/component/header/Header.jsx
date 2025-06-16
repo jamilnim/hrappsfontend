ac6aef3d01f97a9902f76bfe698f94c6388c2b9d
jamilnim <jamilnimbook2@gmail.com>
2025-06-16 21:44:20 +0300

    ..

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -7,7 +7,7 @@ import React from "react";
 function Header({ name }) {
   return (
     <div>
-      <footer className={styles.header}>
+      <header className={styles.header}>
         <div className="logo">
           <img className={styles.logo} src={Em} alt="logo" />
         </div>
@@ -18,7 +18,7 @@ function Header({ name }) {
             <NavLink to="/add">Add Employee</NavLink>
           </ul>
         </nav>
-      </footer>
+      </header>
     </div>
   );
 }
