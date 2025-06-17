af03b14d836ef26fb58c9cb18b02becaed96629f
jamilnim <jamilnimbook2@gmail.com>
2025-06-17 21:07:25 +0300

    backend link update <

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://hrappsbackend.onrender.com",
 });
 
 const useAxios = () => {
